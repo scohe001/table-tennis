defaafd854b067a4250cdbb5136324f306f64529
scohe001 <scohe001@ucr.edu>
2024-12-05 10:41:15 -0500

    Building is different in the new Angular version. Who knew.

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-ng build --output-path docs --base-href ./
+# ng build --output-path docs --base-href ./
+ng build --base-href ./
 cp docs/index.html docs/404.html
 echo lubbocktabletennis.com > docs/CNAME
